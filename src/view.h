fb5e84d966c3c06c654f0d20262b9144f20af999
pandoras <pandoras@edu.21-school.ru>
2025-11-20 01:02:20 +0700

    Code is here

---
+++ src/view.h
@@ -0,0 +1,34 @@
+#ifndef VIEW_H
+#define VIEW_H
+
+#include <QHBoxLayout>
+#include <QImage>
+#include <QLabel>
+#include <QPushButton>
+#include <QVBoxLayout>
+#include <QWidget>
+
+#include "picture.h"
+
+class View : public QWidget {
+ public:
+  explicit View(QWidget *parent = nullptr);
+
+ private:
+  void onLoadImage();
+  void onSaveImage();
+  void updateImageLabel();
+  void updateImageFromPicture();
+
+  QPushButton *btnLoad;
+  QPushButton *btnSave;
+  QPushButton *btnCreateTest;
+  QPushButton *btnAddNoise;
+  QPushButton *btnFilter;
+  QLabel *imageLabel;
+
+  Picture picture;
+  QImage currentImage;
+};
+
+#endif
